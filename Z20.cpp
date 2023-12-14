000656c92584fac94b23f0b832920806a1490a62
n4id4 <117392300+n4id4@users.noreply.github.com>
2023-12-14 12:31:33 +0100

    Update Z20.cpp

--- Z20.cpp
+++ Z20.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 using namespace std;
 
-
-
 bool prost(int a) {
-    for (int i = 2; i < a / 2; i++)
+    for (int i = 2; i <= a / 2; i++)
     {
         if (a % i == 0)
             return false;
     }
     return true;
 }
-
-
 
 int main()
 {
@@ -21,14 +17,13 @@ int main()
     {
         cin >> a >> b;
     } while (a < 10 || b < a + 20);
+    
     if (a % 2 != 0)
-    {
         a++;
-    }
+    
     for (int i = a; i <= b; i += 2)
     {
-        //cout << i<<endl;
-        for (int j = i /2, k = i / 2; j < i; j ++, k --)
+        for (int j = i / 2, k = i / 2; j < i; j++, k--)
         {
             if (prost(j) && prost(k))
             {
